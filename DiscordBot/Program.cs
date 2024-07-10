6b9d476e914d48157caca6ab7ebac104e1c97871
rak3xh <115797199+rak3xh@users.noreply.github.com>
2024-07-10 20:38:53 +0530

    Initial Commit

---
+++ DiscordBot/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Reflection;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DiscordBot
+{
+    internal class Program
+    {
+        private static void Main(string[] args) =>
+            MainAsync(args).GetAwaiter().GetResult();
+
+        private static async Task MainAsync(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddUserSecrets(Assembly.GetExecutingAssembly())
+                .Build();
+
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton<IConfiguration>(configuration)
+                .AddScoped<IBot, Bot>()
+                .BuildServiceProvider();
+
+            try
+            {
+                IBot bot = serviceProvider.GetRequiredService<IBot>();
+
+                await bot.StartAsync(serviceProvider);
+
+                Console.WriteLine("Connected to Discord");
+
+                do
+                {
+                    var keyInfo = Console.ReadKey();
+
+                    if (keyInfo.Key == ConsoleKey.Q)
+                    {
+                        Console.WriteLine("\nShutting down!");
+
+                        await bot.StopAsync();
+                        return;
+                    }
+                } while (true);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+                Environment.Exit(-1);
+            }
+        }
+    }
+}
